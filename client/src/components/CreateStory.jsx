f7fda9c4b67e61ecdea8d37a373efc8f388b13f0
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-03-31 23:49:23 +0300

    initial commit

---
+++ client/src/components/CreateStory.jsx
@@ -0,0 +1,26 @@
+import axios from "axios";
+import React, { useState } from "react";
+
+export default function CreateStory() {
+    const [formData, setFormData] = useState('')
+
+    async function handleSubmit(event) {
+        event.preventDefault()
+        await axios.post('http://localhost:4006/stories', { content: formData })
+        setFormData('')
+    }
+
+    return (
+        <div className="story-form">
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="story">New story:</label>
+                <input
+                    type="text"
+                    value={formData}
+                    onChange={(e) => setFormData(e.target.value)}
+                />
+                <button>submit</button>
+            </form>
+        </div>
+    )
+}
