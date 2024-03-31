688f12bde374a7e6c9fb55744b147c6415017d44
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-03-31 23:54:41 +0300

    initial commit

---
+++ story/index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const axios = require('axios')
+const { randomBytes } = require('crypto')
+const cors = require('cors')
+
+const app = express()
+
+const stories = {}
+
+app.use(cors())
+app.use(express.json())
+
+
+app.post('/stories', async (request, response) => {
+    const id = randomBytes(4).toString('hex')
+    const content = request.body.content
+    stories[id] = { id, content }
+
+    try {
+        await axios.post('http://localhost:4010/events', {
+            type: 'StoryCreation',
+            data: { id, content }
+        })
+    } catch (error) {
+        console.log(error)
+    }
+    response.send({})
+})
+
+app.get('/stories', (request, response) => {
+    response.send(stories)
+})
+
+app.post('/events', (request, response) => {
+    const event = request.body
+    console.log('Recieved event', event.type)
+    response.send({})
+})
+
+app.listen(4006, () => {
+    console.log('server running on port 4006')
+})
