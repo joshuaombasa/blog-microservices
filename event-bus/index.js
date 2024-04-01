ed8875984a231760a6b92e12ee346ea3868f5ed3
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-04-01 03:08:17 +0300

    initial commit

--- event-bus/index.js
+++ event-bus/index.js
@@ -4,25 +4,31 @@ const axios = require('axios')
 
 const app = express()
 
-
+const events = []
 
 
 app.use(express.json())
 
 app.post('/events', async (request, response) => {
     const event = request.body
+    console.log('Recieved event', event.type)
+    events.push(event)
     const { type, data } = event
 
     try {
         await axios.post('http://localhost:4006/events', { type, data })
         await axios.post('http://localhost:4007/events', { type, data })
-        await axios.post('http://localhost:4008/events', { type, data })
         await axios.post('http://localhost:4009/events', { type, data })
+        await axios.post('http://localhost:4008/events', { type, data })
     } catch (error) {
         console.log(error)
     }
 
     response.send({})
+})
+
+app.get('/events', (request,response) => {
+    response.send(events)
 })
 
 
