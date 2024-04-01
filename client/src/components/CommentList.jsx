ad4d74d710e1f87bd34822a9b08fc253afd9db80
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-04-01 03:07:58 +0300

    initial commit

--- client/src/components/CommentList.jsx
+++ client/src/components/CommentList.jsx
@@ -2,11 +2,27 @@ import React from "react";
 
 export default function CommentList({ comments }) {
 
-    const renderComments = comments.map(comment => (
-        <li key={comment.id}>
-            <em>{comment.comment}</em>
+    const renderComments = comments.map(comment => {
+
+        let content = ''
+        let statusStyle
+        if (comment.status === 'approved') {
+            content = comment.comment
+            statusStyle = {color: 'green', fontWeight:'700'}
+        } else if (comment.status === 'pending') {
+            content = 'This comment awaiting moderation'
+            statusStyle={color: 'purple'}
+        } else if (comment.status === 'rejected') {
+            content = 'This comment has been rejected'
+            statusStyle={color: 'red',textDecoration:'underline'}
+        }
+  
+    
+        return (<li style={statusStyle} key={comment.id}>
+            <em>{content}</em>
         </li>)
-    )
+
+    })
     return (
         <div className="comment-list">
             {renderComments}
