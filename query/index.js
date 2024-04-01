f8615ec57016cfa419b197b4d4377f17f9e4ae0b
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-04-01 03:08:38 +0300

    initial commit

--- query/index.js
+++ query/index.js
@@ -1,11 +1,31 @@
 const express = require('express')
 const cors = require('cors')
+const axios = require('axios')
 const app = express()
 
 const stories = {}
 
 app.use(cors())
 app.use(express.json())
+
+const handleEvents = (type, data) => {
+    if (type === 'StoryCreation') {
+        const { id, content } = data
+        stories[id] = { postId: id, content, comments: [] }
+
+    } else if (type === 'CommentCreation') {
+        const { postId, id, comment, status } = data
+        const comments = stories[postId].comments
+        comments.push({ id, comment, status })
+        stories[postId].comments = comments
+
+    } else if (type === 'CommentUpdated') {
+        const { postId, id, comment, status } = data
+        const comments = stories[postId].comments
+        const commentToUpdate = comments.find(comment => comment.id === id)
+        commentToUpdate.status = status
+    }
+}
 
 app.get('/stories', (request, response) => {
     response.send(stories)
@@ -16,28 +36,16 @@ app.post('/events', (request, response) => {
     console.log('Recieved event', event.type)
 
     const { type, data } = event
-    if (type === 'StoryCreation') {
-        const { id, content } = data
-        stories[id] = { postId: id, content, comments: [] }
-
-        response.send({})
-    } else if (type === 'CommentCreation') {
-        const { postId, id, comment, status } = data
-        const comments = stories[postId].comments
-        comments.push({ id, comment, status })
-        stories[postId].comments = comments
-
-        response.send({})
-    } else if (type === 'CommentUpdated') {
-        const { postId, id, comment, status } = data
-        const comments = stories[postId].comments
-        comments.push({ id, comment, status })
-        stories[postId].comments = comments
-
-        response.send({})
-    }
+    handleEvents(type, data)
+    response.send({})
 })
 
-app.listen(4008, () => {
+app.listen(4008, async () => {
     console.log('server running on port 4008')
+
+    const res = await axios.get('http://localhost:4010/events')
+    for (let event of res.data) {
+        const { type, data } = event
+        handleEvents(type, data)
+    }
 })
